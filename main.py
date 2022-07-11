0b177a8c7da321778c847153212752f0b015fff2
Ziqi Guo <guoziqi2018@outlook.com>
2022-07-11 08:19:29 -0400

    test

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from random import random
 from RandomFuzzer import RandomFuzzer
+from MutationFuzzer import MutationFuzzer
 from ProgramRunner import ProgramRunner
 import sys
 
 
 def fuzzing(program: str = "./test", min_length: int = 10, max_length: int = 100, trials: int = 10):
-    random_fuzzer = RandomFuzzer(min_length=min_length, max_length=max_length)
+    inp = open("tests/csv1.txt").read()
     p = ProgramRunner(program)
+    mutation_fuzzer = MutationFuzzer(seed=inp)
+
 
     for i in range(trials):
-        print(random_fuzzer.run(p)[1])
+        print(mutation_fuzzer.run(p)[1])
 
 
 if __name__ == "__main__":
-    fuzzing(sys.argv[1], min_length=10, max_length=5000, trials=100)
+    fuzzing("./tests/csv1", min_length=10, max_length=100000, trials=20)
     
 

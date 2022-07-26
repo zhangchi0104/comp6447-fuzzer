290285b9d9f04cacd1a953a0b6c4d510edbd8246
z5308709 <z5308709@cse.unsw.edu.au>
2022-07-26 16:00:23 +1000

    embedded mutator class

--- mutators/json_mutation_fuzzer.py
+++ mutators/json_mutation_fuzzer.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 import json
 import random
 import pwnlib.util.fiddling as bits
+from .mutator_base import MutatorBase
 
 def generate(size: int):
     ''' Generate JSONs within JSON '''
@@ -15,19 +16,23 @@ def generate(size: int):
     
     return json.dumps(badJson)
 
-class jsonMutationFuzzer(object):
+class jsonMutationFuzzer(MutatorBase):
   def __init__(self, seed):
       """
       args:
           seed: the seed csv bytes to mutate from
       """
-      self._content = seed
+      super().__init__(seed)
+      self._content = json.loads(seed)
+      
   
-  def _bit_flip(self):
-      sampleInput = self._content.decode()
-  
+  def _mutate_bit_flip(self):
+      sampleInput = json.dumps(self._content)
+      pos = random.randint(0, len(sampleInput) - 1)
+      
       for _ in range(0, 500):
           # convert the input into a bytearray
+          #print(sampleInput)
           b = bytearray(sampleInput, 'UTF-8')
   
           # Then we search through the entire bytearray created, and randomly
@@ -36,13 +41,12 @@ class jsonMutationFuzzer(object):
               if random.randint(0, 20) == 1:
                   b[i] ^= random.getrandbits(7)
   
-          # Once we have flipped the bits, we want to decode this back into a string
-          # that can be passed in as input to the binary
-          #mutatedInput = b.decode('ascii').strip()
-          
+      # Once we have flipped the bits, we want to decode this back into a string
+      # that can be passed in as input to the binary
+      #mutatedInput = b.decode('ascii').strip()
       return b
 
-  def fuzzJson(self):
+  def _mutate_fuzzJson(self):
     
     for i in range(10):
         badJson = generate(i).replace('\\"',"\"")
@@ -50,21 +54,6 @@ class jsonMutationFuzzer(object):
     return mutatedInput
   
   
-  def __iter__(self):
-      return self
-
-  def __next__(self):
-      # randomly select a mutations methods
-      # inspired from ziqi's code
-      choice = random.choice([
-          self._bit_flip,
-          self.fuzzJson
-          # self._delete_random_byte,
-          # self._empty_csv_random_rows,
-          # self._csv_random_rows,
-          # self._csv_magic_rows
-      ])
-      # make mutation
-      raw = choice()
-      # construct the csv file body
-      return raw
+  def format_output(self, raw):
+        # construct the csv file body
+        return raw
